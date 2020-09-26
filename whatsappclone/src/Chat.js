edcdededd7c6235cda4155e22e713b7d5b77024a
stamic1 <bhatgaurav96@gmail.com>
2020-09-26 14:00:58 +0530

    ux done

---
+++ whatsappclone/src/Chat.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import AttachFile from "@material-ui/icons/AttachFile";
+import MoreVert from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+
+export default function Chat() {
+  const [seed, setSeed] = useState("");
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at .....</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className={`chat__message  ${true && "chat__receiver"}`}>
+          <span className="chat__name">Gaurav Bhat</span>
+          hello Guys
+          <span className="chat__timestamp">3:52pm</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            type="text"
+            placeholder="Type a message"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={sendMessage} type="submit">
+            Send Message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
+}
