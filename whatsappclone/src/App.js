36225d56c6dbc268ade9c3ff580c118b40dce7c4
stamic1 <bhatgaurav96@gmail.com>
2020-09-26 09:40:21 +0530

    intial checkin + clean up

---
+++ whatsappclone/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    //BEM naming convention
+    <div className="app">
+    <h1>lets build whats app clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ whatsappclone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCq9cmMUEfg2DSr2JjeRGqJawZqth7LR0g",
+    authDomain: "whats-app-clone-b75de.firebaseapp.com",
+    databaseURL: "https://whats-app-clone-b75de.firebaseio.com",
+    projectId: "whats-app-clone-b75de",
+    storageBucket: "whats-app-clone-b75de.appspot.com",
+    messagingSenderId: "20767238248",
+    appId: "1:20767238248:web:a3c1f0003c1c57795db3dd",
+    measurementId: "G-WQZTGT4FT8"
+  };
