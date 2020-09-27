316106f9360dbd86a9b85fbfe70808525c7ea543
stamic1 <bhatgaurav96@gmail.com>
2020-09-27 10:54:23 +0530

    Chat window last seen

--- whatsappclone/src/Chat.js
+++ whatsappclone/src/Chat.js
@@ -55,7 +55,11 @@ export default function Chat() {
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerInfo">
           <h3>{roomName}</h3>
-          <p>Last seen at .....</p>
+          <p>
+            {new Date(
+              messages[messages.length - 1]?.timestamp?.toDate()
+            ).toUTCString()}
+          </p>
         </div>
         <div className="chat__headerRight">
           <IconButton>
