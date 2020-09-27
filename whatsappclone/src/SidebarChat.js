abbec27851e74119ae6ff62a99eb14133c45965c
stamic1 <bhatgaurav96@gmail.com>
2020-09-27 11:04:13 +0530

    fix

--- whatsappclone/src/SidebarChat.js
+++ whatsappclone/src/SidebarChat.js
@@ -5,12 +5,23 @@ import db from "./firebase";
 import { Link } from "react-router-dom";
 export default function SidebarChat({ id, name, addNewChat }) {
   const [seed, setSeed] = useState("");
-
+  const [messages, setMessages] = useState("");
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
 
- 
+  useEffect(() => {
+    if (id) {
+      db.collection("rooms")
+        .doc(id)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+    }
+  }, []);
+
   const createChat = () => {
     const roomName = prompt("Please eneter name for chat room");
 
@@ -27,7 +38,7 @@ export default function SidebarChat({ id, name, addNewChat }) {
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="sidebarChat__info">
           <h2>{name}</h2>
-          <p>Last Message..</p>
+          <p>{messages[0]?.message}</p>
         </div>
       </div>
     </Link>
